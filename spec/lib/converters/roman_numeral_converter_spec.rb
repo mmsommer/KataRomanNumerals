e9dfe9c505d7d2339be49bddf4ac419a6397c9cb
Michael Sommer <michael.sommer@ogd.nl>
2014-04-18 10:24:54 +0200

    convert 10 to roman 'X' convert 50 to roman 'L' convert 100 to roman 'C' convert 500 to roman 'D' convert 1000 to roman 'M'

--- spec/lib/converters/roman_numeral_converter_spec.rb
+++ spec/lib/converters/roman_numeral_converter_spec.rb
@@ -67,6 +67,26 @@ describe 'Roman Numeral Converter' do
       it 'should convert 5 to V' do
         converter.convert_to_roman(5).should eql 'V'
       end
+
+      it 'should convert 10 to X' do
+        converter.convert_to_roman(10).should eql 'X'
+      end
+
+      it 'should convert 50 to L' do
+        converter.convert_to_roman(50).should eql 'L'
+      end
+
+      it 'should convert 100 to C' do
+        converter.convert_to_roman(100).should eql 'C'
+      end
+
+      it 'should convert 500 to D' do
+        converter.convert_to_roman(500).should eql 'D'
+      end
+
+      it 'should convert 1000 to M' do
+        converter.convert_to_roman(1000).should eql 'M'
+      end
     end
   end
 end
