04b8ac974614b0cec9ab6b186e9b011b0391663d
Michael Sommer <michael.sommer@ogd.nl>
2014-04-18 10:00:45 +0200

    Whoopsie convert_to_roman should have been convert_to_digit ofcourse.

--- lib/converters/roman_numeral_converter.rb
+++ lib/converters/roman_numeral_converter.rb
@@ -1,6 +1,6 @@
 module Converters
   class RomanNumeralConverter
-    def convert_to_roman(roman)
+    def convert_to_digit(roman)
       roman = roman.chars.to_a
 
       case
@@ -9,9 +9,9 @@ module Converters
         when roman.size == 1
           ROMAN[roman.shift]
         when ROMAN[roman[0]] < ROMAN[roman[1]]
-          -ROMAN[roman.shift] + convert_to_roman(roman.join)
+          -ROMAN[roman.shift] + convert_to_digit(roman.join)
         else
-          ROMAN[roman.shift] + convert_to_roman(roman.join)
+          ROMAN[roman.shift] + convert_to_digit(roman.join)
       end
     end
 
--- spec/lib/converters/roman_numeral_converter_spec.rb
+++ spec/lib/converters/roman_numeral_converter_spec.rb
@@ -4,56 +4,56 @@ require 'converters/roman_numeral_converter'
 describe 'Roman Numeral Converter' do
   let (:converter) { Converters::RomanNumeralConverter.new }
 
-  describe 'convert_to_roman' do
+  describe 'convert_to_digit' do
     context 'Single Roman numbers' do
       it 'should convert I to 1' do
-        converter.convert_to_roman('I').should eql 1
+        converter.convert_to_digit('I').should eql 1
       end
 
       it 'should convert V to 5' do
-        converter.convert_to_roman('V').should eql 5
+        converter.convert_to_digit('V').should eql 5
       end
 
       it 'should convert X to 10' do
-        converter.convert_to_roman('X').should eql 10
+        converter.convert_to_digit('X').should eql 10
       end
 
       it 'should convert L to 50' do
-        converter.convert_to_roman('L').should eql 50
+        converter.convert_to_digit('L').should eql 50
       end
 
       it 'should convert C to 100' do
-        converter.convert_to_roman('C').should eql 100
+        converter.convert_to_digit('C').should eql 100
       end
 
       it 'should convert D to 500' do
-        converter.convert_to_roman('D').should eql 500
+        converter.convert_to_digit('D').should eql 500
       end
 
       it 'should convert M to 1000' do
-        converter.convert_to_roman('M').should eql 1000
+        converter.convert_to_digit('M').should eql 1000
       end
     end
 
     context 'Extra roman number on the right' do
       it 'should convert VI to 6' do
-        converter.convert_to_roman('VI').should eql 6
+        converter.convert_to_digit('VI').should eql 6
       end
 
       it 'should convert VII to 7' do
-        converter.convert_to_roman('VII').should eql 7
+        converter.convert_to_digit('VII').should eql 7
       end
     end
 
     context 'Extra roman number on the left' do
       it 'should convert IV to 4' do
-        converter.convert_to_roman('IV').should eql 4
+        converter.convert_to_digit('IV').should eql 4
       end
     end
 
     context 'Now a hard one: MCMLXXXIV' do
       it 'should convert to 1984' do
-        converter.convert_to_roman('MCMLXXXIV').should eql 1984
+        converter.convert_to_digit('MCMLXXXIV').should eql 1984
       end
     end
   end
